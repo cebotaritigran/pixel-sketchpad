5a9a15d0a626353938f8e27cfff10f5d512b69a9
CebotariTigran <cebotaritigran@gmail.com>
2023-02-23 21:07:19 +0100

    Add on toggle sketching

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
+const gridContainer = document.querySelector('#gridContainer');
+let gridSystem = 16*16;
 function createGrid() {
     const gridContainer = document.querySelector('#gridContainer');
-
-    for (let i = 1; i < 257; i++) {
+    for (let i = 0; i < gridSystem; i++) {
         const div = document.createElement('div');
-        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        div.style.cssText = "border: 0px solid black; height: 25px; width: 25px";
         div.classList.add('gridEffect', 'gridBox');
-        div.setAttribute("draggable", "false");
+        div.setAttribute("id", i);
         gridContainer.appendChild(div);
     }
 }
 
 createGrid();
-
-function paintGrid() {
-    const gridBox = document.querySelectorAll('.gridBox');
-    function paintGridBox() {
-        this.classList.add('painted');
+let x = false;
+    function click() {
+        if(x == true) {
+            x = false;
+        } else {
+            x = true;
+        }
     }
+function paintGrid() {
+    const body = document.querySelector('body');
+    const gridBox = document.querySelectorAll('.gridBox');
+    
+    
+    function paintGridBox() {
+        if(x == true){
+            this.classList.add('painted');
+        }
+        
+    }
+    for(let i = 0; i < gridSystem; i++){
+        gridBox[i].addEventListener('mouseover', paintGridBox);
+    }
+    
 
+    /*
     for (const box of gridBox) {
         box.addEventListener('mouseover', paintGridBox);
     }
+    */
+
 }
 
+
 paintGrid();
+window.addEventListener('click', click)
 

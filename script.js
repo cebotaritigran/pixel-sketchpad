b3619e91db8a5abf8344bbd76c7b282392877f90
CebotariTigran <cebotaritigran@gmail.com>
2023-02-25 15:13:40 +0100

    Add eraser

--- script.js
+++ script.js
@@ -2,28 +2,55 @@ const gridSize = document.querySelector('#gridSize');
 const gridSizeShow = document.querySelector('#gridSizeShow');
 const gridContainer = document.querySelector('#gridContainer');
 
+// Checking if the mouse button is down
 let mouseClick = false;
 
-function click() {
+function mouseDown() {
     mouseClick = true;
 }
 
-function click2() {
+function mouseUp() {
     mouseClick = false;
 }
 
-function paintGrid(gridSystem) {
+const gridBoxSelect = document.querySelector('.gridContainer');
+gridBoxSelect.addEventListener('mousedown', mouseDown)
+gridBoxSelect.addEventListener('mouseup', mouseUp)
+
+// Adds a class to each and every div we created only if mouse button is down and if mouse is over that div
+// which we check with event listener on that div
+// and paint the that div to black or erase if eraser button is on
+function manipulateGrid(gridSystem) {
     const gridBox = document.querySelectorAll('.gridBox');
-    function paintGridBox() {
+    function manipulateGridBox() {
         if (mouseClick == true) {
             this.classList.add('paintedGrid');
         }
+        if(mouseClick == true && eraserOn == true){
+            this.classList.remove('paintedGrid');
+        }
     }
     for (let i = 0; i < gridSystem; i++) {
-        gridBox[i].addEventListener('mouseover', paintGridBox);
+        gridBox[i].addEventListener('mouseover', manipulateGridBox);
     }
 }
 
+let eraserOn = false;
+
+function eraseGrid() {
+    if (eraserOn) {
+        eraserOn = false;
+    } else {
+        eraserOn= true;
+    }
+}
+
+const eraser = document.querySelector('#eraser');
+eraser.addEventListener('click', eraseGrid)
+
+
+// we add an event listener to our input range html element and whenever there is an input we go ahead and clear
+// all the divs that were created when page is load in default and the we create all divs again with given size
 gridSize.addEventListener('input', function (e) {
     gridContainer.replaceChildren();
     let gridSystem = gridSize.value * gridSize.value
@@ -34,7 +61,7 @@ gridSize.addEventListener('input', function (e) {
         div.classList.add('gridBox');
         gridContainer.appendChild(div);
     }
-    paintGrid(gridSystem);
+    manipulateGrid(gridSystem);
 });
 
 function createGrid() {
@@ -45,13 +72,8 @@ function createGrid() {
         div.classList.add('gridBox');
         gridContainer.appendChild(div);
     }
-    paintGrid(256);
+    manipulateGrid(256);
 }
 
 createGrid();
-
-const gridBoxSelect = document.querySelector('.gridContainer');
-
-gridBoxSelect.addEventListener('mousedown', click)
-gridBoxSelect.addEventListener('mouseup', click2)
 
